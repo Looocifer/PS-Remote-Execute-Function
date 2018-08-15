ed6af3d22c5d5b8be7f7bb1bc211f931acaa0492
Prithu Ganguli <ganguli.prithu@hotmail.com>
2018-08-15 18:32:12 +0530

    Update RemoteExeVar.ps1

--- RemoteExeVar.ps1
+++ RemoteExeVar.ps1
@@ -7,6 +7,6 @@ $Command = 'ENTER_COMMAND_HERE'
 $RemoteComputers = Get-Content $InputFile
 ForEach ($RemoteComputer in $RemoteComputers) {
         'Next Output From Server ' + $RemoteComputer | Out-File -FilePath $OutputFile -Append
-        Invoke-Command -Computername $RemoteComputer -ScriptBlock {Invoke Expression -Command $Command} | Out-File -FilePath $OutputFile -Append
+        Invoke-Command -Computername $RemoteComputer -ScriptBlock {Invoke-Expression -Command $Command} | Out-File -FilePath $OutputFile -Append
 }
 notepad.exe $OutputFile 
